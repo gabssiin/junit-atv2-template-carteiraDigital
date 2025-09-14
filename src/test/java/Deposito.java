20c254305cec6b0c139fd7a0c2f98c0ecc008bce
gabssiin <gabriel.willian.br@gmail.com>
2025-09-14 17:11:50 -0300

    Adiciona testes para a classe DigitalWallet e atualiza dependências no pom.xml

--- src/test/java/Deposito.java
+++ src/test/java/Deposito.java
@@ -1,20 +1,38 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import com.example.DigitalWallet;
 
-
 class Deposito {
-        
-        void deveDepositarValoresValidos(double amount) {
-            
-        }
 
-        
-        void deveLancarExcecaoParaDepositoInvalido(double amount) {
-            
-        }
+    DigitalWallet dw;
+
+    @BeforeEach
+    public void setUp() {
+        dw = new DigitalWallet("a", 0);
     }
+
+    @ParameterizedTest
+    @ValueSource(doubles = { 10.0, 50.0, 100.0 })
+    void deveDepositarValoresValidos(double amount) {
+        double saldoInicial = dw.getBalance();
+        dw.deposit(amount);
+
+        double saldoEsperado = saldoInicial + amount;
+        Assertions.assertEquals(saldoEsperado, dw.getBalance(), 0.001,
+                "Amount must be > 0");
+    }
+
+    @Test
+    void deveLancarExcecaoParaDepositoInvalido() {
+        IllegalArgumentException excecao = assertThrows(IllegalArgumentException.class,
+                () -> dw.deposit(-100.0));
+        assertEquals("Amount must be > 0", excecao.getMessage());
+    }
+}
--- src/test/java/SaldoInicial.java
+++ src/test/java/SaldoInicial.java
@@ -1,16 +1,31 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.example.DigitalWallet;
 
 class SaldoInicial {
-        void deveConfigurarSaldoInicialCorreto() {
-            
-        }
+    DigitalWallet dw;
 
-        void deveLancarExcecaoParaSaldoInicialNegativo() {
-            
-        }
+    @BeforeEach
+    public void setUp() {
+        dw = new DigitalWallet("a", 0);
     }
+
+    @Test // V
+    void deveConfigurarSaldoInicialCorreto() {
+        assertEquals(0, dw.getBalance());
+    }
+
+    @Test
+    void deveLancarExcecaoParaSaldoInicialNegativo() {
+    IllegalArgumentException excecao = assertThrows(IllegalArgumentException.class,
+                () -> new DigitalWallet("a", -1));
+            Assertions.assertEquals("Negative initial balance", excecao.getMessage());
+            }
+
+
+}
